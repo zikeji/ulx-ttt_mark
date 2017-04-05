cd5fc69a9788c07fa79ab8419f62146c08490eb5
= <caleb@zynados.com>
2017-04-05 16:32:04 -0400

    better label support

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
 # ULX ttt_mark
 
-Small ULX utility to add a "!mark" or "ulx mark" command that functions similar to ttt_radio, except will mark the target as specified without needing to mark them on the scoreboard by finding and clicking the name.
+git remote add origin https://github.com/zikeji/ulx-ttt_mark.git
+git push -u origin master
 
 ## Installation
 
 Install as you would a normal ULX addon.
+
+## Usage
+
+The command is available to all players. If no target is specified, will mark the person the player is looking at, if valid.
+
+### From Chat
+
+- Mark player as friendly
+ - `!mark f [player]`
+ - `!mark friend [player]`
+ - `!mark friendly [player]`
+- Mark player as suspicious
+ - `!mark s [player]`
+ - `!mark susp [player]`
+ - `!mark suspicious [player]`
+- Mark player as avoid
+ - `!mark a [player]`
+ - `!mark avoid [player]`
+- Mark player as kill
+ - `!mark k [player]`
+ - `!mark kill [player]`
+- Mark player as kill
+ - `!mark m [player]`
+ - `!mark miss [player]`
+ - `!mark missing [player]`
+
+### From Console
+You can use the short forms (f, m, etc.) however console does have ULX autocomplete support.
+
+- Mark player as friendly
+ - `ulx_mark friendly [player]`
+- Mark player as suspicious
+ - `ulx_mark suspicious [player]`
+- Mark player as avoid
+ - `ulx_mark avoid [player]`
+- Mark player as kill
+ - `ulx_mark kill [player]`
+- Mark player as kill
+ - `ulx_mark missing [player]`
--- lua/ulx/modules/sh/ttt_mark.lua
+++ lua/ulx/modules/sh/ttt_mark.lua
@@ -1,6 +1,6 @@
 if CLIENT then
 
-    function ulx.cl_ttt_mark( target, tag )
+    function ulx.cl_ttt_mark( target, mark_type )
         if not target then
             local current, vague = RADIO:GetTarget() --see cl_voice.lua, "current" won't be valid if disguised, dead
             print(current)
@@ -8,8 +8,44 @@ if CLIENT then
             if vague then return end
             target = current
         end
-        
-        target.sb_tag = tag
+
+        local tags = { --found in sb_info.lua, because it is local we unfortunately have to redeclare them
+            {txt="sb_tag_friend", color=COLOR_GREEN},
+            {txt="sb_tag_susp",   color=COLOR_YELLOW},
+            {txt="sb_tag_avoid",  color=Color(255, 150, 0, 255)},
+            {txt="sb_tag_kill",   color=COLOR_RED},
+            {txt="sb_tag_miss",   color=Color(130, 190, 130, 255)}
+        }
+
+        local mark_types = {
+            {mark_type="f", tag=tags[1]},
+            {mark_type="friend", tag=tags[1]},
+            {mark_type="friendly", tag=tags[1]},
+
+            {mark_type="s", tag=tags[2]},
+            {mark_type="susp", tag=tags[2]},
+            {mark_type="suspicious", tag=tags[2]},
+
+            {mark_type="a", tag=tags[3]},
+            {mark_type="avoid", tag=tags[3]},
+
+            {mark_type="k", tag=tags[4]},
+            {mark_type="kill", tag=tags[4]},
+
+            {mark_type="m", tag=tags[5]},
+            {mark_type="miss", tag=tags[5]},
+            {mark_type="missing", tag=tags[5]}
+        }
+
+        local tag = nil
+
+        for i,v in ipairs(mark_types) do
+            if v.mark_type == mark_type then
+                target.sb_tag = v.tag
+                print(v.tag)
+                break
+            end
+        end
     end
 
 end
@@ -22,23 +58,12 @@ function ulx.ttt_mark( calling_ply, mark_type, target )
     if target == calling_ply then -- this means they didn't specify a target
         target = nil
     end
-
-    -- because tags in sb_info.lua is local we have to redeclare them - bah!
-    local tag = nil
-
-    if mark_type == "friendly" then tag = {txt="sb_tag_friend", color=COLOR_GREEN}
-    elseif mark_type == "suspicious" then tag = {txt="sb_tag_susp",   color=COLOR_YELLOW}
-    elseif mark_type == "avoid" then tag = {txt="sb_tag_avoid",  color=Color(255, 150, 0, 255)}
-    elseif mark_type == "kill" then tag = {txt="sb_tag_kill",   color=COLOR_RED}
-    elseif mark_type == "missing" then tag = {txt="sb_tag_miss",   color=Color(130, 190, 130, 255)}
-    else return
-    end
     
-    ULib.clientRPC( calling_ply, "ulx.cl_ttt_mark", target, tag )
+    ULib.clientRPC( calling_ply, "ulx.cl_ttt_mark", target, mark_type )
 end
 
 local ttt_mark = ulx.command( "TTT Utility", "ulx mark", ulx.ttt_mark, {"!mark"} )
-ttt_mark:addParam{ type=ULib.cmds.StringArg, help="label", completes={"friendly", "suspicious", "avoid", "kill", "missing"}, ULib.cmds.restrictToCompletes }
+ttt_mark:addParam{ type=ULib.cmds.StringArg, help="label", completes={"friendly", "suspicious", "avoid", "kill", "missing"} }
 ttt_mark:addParam{ type=ULib.cmds.PlayerArg, ULib.cmds.optional }
 ttt_mark:defaultAccess( ULib.ACCESS_ALL )
 ttt_mark:help( "Marks the targeted player." )
